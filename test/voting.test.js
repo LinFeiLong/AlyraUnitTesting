2096aae412c8948301e704255f67c1c4f5f8508c
Fernand Lime <fernand.lime@gmail.com>
2022-11-01 16:46:14 +0100

    add testing

--- test/voting.test.js
+++ test/voting.test.js
@@ -5,55 +5,38 @@ const { expect } = require("chai")
 
 contract("Voting", (accounts) => {
   const _owner = accounts[0]
+  const _second = accounts[1]
+  const _third = accounts[2]
 
   let VotingInstance
 
   beforeEach(async function () {
-    VotingInstance = await Voting.new()
-    // VotingInstance.voters[_owner].isRegistered = true
-  })
-  it("has a default workflowStatus set to RegisteringVoters", async () => {
-    const status = await VotingInstance.workflowStatus()
-    const WorkflowStatus = VotingInstance.constructor.WorkflowStatus
-
-    expect(status).to.be.bignumber.equal(
-      new BN(WorkflowStatus.RegisteringVoters)
-    )
+    VotingInstance = await Voting.new({ from: _owner })
   })
 
-  it("can change state", async () => {
-    // it("changes state", async () => {
-    //   expect(await VotingInstance.workflowStatus()).to.be.bignumber.equal(
-    //     new BN(
-    //       VotingInstance.constructor.WorkflowStatus.ProposalsRegistrationStarted
-    //     )
-    //   )
-    // })
-    // it("sets workflowStatus to ProposalsRegistrationEnded", async () => {
-    //   expect(await VotingInstance.workflowStatus()).to.be.bignumber.equal(
-    //     new BN(
-    //       VotingInstance.constructor.WorkflowStatus.ProposalsRegistrationEnded
-    //     )
-    //   )
-    // })
-    // it("sets workflowStatus to VotingSessionStarted", async () => {
-    //   expect(await VotingInstance.workflowStatus()).to.be.bignumber.equal(
-    //     new BN(VotingInstance.constructor.WorkflowStatus.VotingSessionStarted)
-    //   )
-    // })
-    // it("sets workflowStatus to VotingSessionEnded", async () => {
-    //   expect(await VotingInstance.workflowStatus()).to.be.bignumber.equal(
-    //     new BN(VotingInstance.constructor.WorkflowStatus.VotingSessionEnded)
-    //   )
-    // })
-    // it("sets workflowStatus to VotesTallied", async () => {
-    //   expect(await VotingInstance.workflowStatus()).to.be.bignumber.equal(
-    //     new BN(VotingInstance.constructor.WorkflowStatus.VotesTallied)
-    //   )
-    // })
-  })
+  describe("getVoter", function () {
+    it("should get a voter", async () => {
+      // On ajoute l'owner en tant qu' electeur car la fonction getVoter
+      // ne peut être utilisé que par un electeur
+      await VotingInstance.addVoter(_owner)
 
-  // it("can add a voter", async () => {
-  //   expect(await VotingInstance.getVoter(_owner)).to.be.equal(null)
-  // })
+      // On vérifie que l'owner fait partie de la liset des electeurs
+      const findEvent = await VotingInstance.getVoter(_owner)
+
+      // On est sensé trouvé un resultat
+      expect(response).not.to.be.false
+    })
+
+    it("should not get a voter", async () => {
+      // On ajoute l'owner en tant qu' electeur car la fonction getVoter
+      // ne peut être utilisé que par un electeur
+      await VotingInstance.addVoter(_owner)
+
+      // On vérifie que _second fait partie de la liset des electeurs
+      const response = await VotingInstance.getVoter(_second)
+
+      // On est sensé trouvé un resultat
+      expect(response).to.be.false
+    })
+  })
 })
